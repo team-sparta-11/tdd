e717326bbe73c9a6e854bcc27c84aaee804b492b
sunday00 <kind65505256@gmail.com>
2023-12-09 17:25:19 +0900

    :wrench: Chore: working... config by env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configModuleOption } from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(configModuleOption)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import swaggerConfig from './config/swagger.config';
+import { ConfigService } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
   swaggerConfig(app);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get('appConfig');
+
+  await app.listen(appConfig['port']);
+
+  return appConfig;
 }
 
-bootstrap().then(() => {
-  console.log('3000');
+bootstrap().then((appConfig) => {
+  console.log(appConfig['port']);
 });
