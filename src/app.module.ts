4d7d6ca232b7c6fca8ce210bc4005bdd459f4763
sunday00 <kind65505256@gmail.com>
2023-12-10 16:06:29 +0900

    :memo: Chore: api summary
    임시 api 명세 - 과제 제출용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOption } from './config/app.config';
+import { TmpApisModule } from './tmp-apis/tmp-apis.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOption)],
+  imports: [ConfigModule.forRoot(configModuleOption), TmpApisModule],
   controllers: [AppController],
   providers: [AppService],
 })
