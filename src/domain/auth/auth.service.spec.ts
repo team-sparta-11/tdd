ae2d310ee9554c7a734112a328f7853a356e6a83
sunday00 <kind65505256@gmail.com>
2023-12-12 02:15:07 +0900

    :white_check_mark: test: auth service

--- src/domain/auth/auth.service.spec.ts
+++ src/domain/auth/auth.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
+const mockRepository = {
+  create: jest.fn(),
+  save: async () => jest.fn(),
+};
+
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: 'UserRepository', useValue: mockRepository },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
