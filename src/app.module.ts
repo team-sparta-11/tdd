3a343f4a0587a23d75ed2814fdf4132b9d89ea7f
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-12 07:26:13 +0900

    feat: add empty auth module service controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { configModuleOption } from './config/app.config';
 import { TmpApisModule } from './tmp-apis/tmp-apis.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOption),
     TmpApisModule,
     TypeOrmModule.forRootAsync(typeORMConfig),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
