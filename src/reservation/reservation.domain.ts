2faef63bbd2eabf952dda38df057a5eade8a84b6
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-22 07:28:05 +0900

    fix: change userid and seatnumber from string to number

--- src/reservation/reservation.domain.ts
+++ src/reservation/reservation.domain.ts
@@ -2,9 +2,9 @@ import { PAYMENT_STATUS } from 'src/common/types/reservation';
 
 export interface Reservation {
   id: number;
-  userId: string;
+  userId: number;
   date: string;
-  seatNumber: string;
+  seatNumber: number;
   isExpired: boolean;
   paymentStatus: PAYMENT_STATUS;
 }
