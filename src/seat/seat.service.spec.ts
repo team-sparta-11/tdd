df25b802b890d40868aee1ac23d041bfe9153c81
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-29 18:24:36 +0900

    feat: add seat service test

--- src/seat/seat.service.spec.ts
+++ src/seat/seat.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeatService } from './seat.service';
+import { SeatReader } from './seat.handler';
 
 describe('SeatService', () => {
   let service: SeatService;
+  let reader: SeatReader;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [{ provide: SeatService, useValue: jest.fn() }],
+      providers: [
+        SeatService,
+        {
+          provide: SeatReader,
+          useValue: {
+            getAvailableSeatByDate: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SeatService>(SeatService);
+    reader = module.get<SeatReader>(SeatReader);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get available seat by date correctly', async () => {
+    const date = '2024-01-01';
+    const seats = [1, 2, 3, 4, 5];
+
+    const getAvailableSeatSpy = jest
+      .spyOn(reader, 'getAvailableSeatByDate')
+      .mockResolvedValue(seats);
+
+    const result = await service.getAvailableSeatsByDate(date);
+
+    expect(result).toEqual(seats);
+    expect(getAvailableSeatSpy).toHaveBeenCalledWith(date);
   });
 });
