3780b898e1d7f55bdd165ae1c544b1d540c1b251
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-29 18:17:14 +0900

    feat: add reservation service test except batch

--- src/reservation/reservation.service.spec.ts
+++ src/reservation/reservation.service.spec.ts
@@ -1,22 +1,112 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
 import { ReservationService } from './reservation.service';
-import { SeatService } from 'src/seat/seat.service';
+import { ReservationManager, ReservationReader } from './reservation.handler';
+import { RequestReservationDto } from './dto/request-reservation.dto';
+import { SeatManager, SeatReader } from 'src/seat/seat.handler';
+import { Seat } from 'src/seat/struct/seat.domain';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 describe('ReservationService', () => {
-  let service: ReservationService;
+  let reservationService: ReservationService;
+  let seatReader: SeatReader;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        { provide: ReservationService, useValue: jest.fn() },
-        { provide: SeatService, useValue: jest.fn() },
+        ReservationService,
+        {
+          provide: ReservationManager,
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: ReservationReader,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: SeatManager,
+          useValue: {
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: SeatReader,
+          useValue: {
+            getSeat: jest.fn(),
+          },
+        },
+        {
+          provide: DataSource,
+          useValue: {
+            createQueryRunner: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
-    service = module.get<ReservationService>(ReservationService);
+    reservationService = module.get<ReservationService>(ReservationService);
+    seatReader = module.get<SeatReader>(SeatReader);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should throw BadRequestException if there is no seat', async () => {
+    const wrongRequestReservationDto = {
+      seatNumber: 0,
+      date: '2024-01-01',
+    };
+    const getSeatSpy = jest
+      .spyOn(seatReader, 'getSeat')
+      .mockResolvedValue(undefined);
+
+    await expect(
+      reservationService.requestReservation({
+        requestReservationDto: wrongRequestReservationDto,
+        userId: 1,
+      }),
+    ).rejects.toThrow(new BadRequestException('There is no seat'));
+
+    expect(getSeatSpy).toHaveBeenCalledWith({
+      seatNumber: wrongRequestReservationDto.seatNumber,
+      dateAvailability: { date: wrongRequestReservationDto.date },
+    });
+  });
+
+  it('should throw InternalServerErrorException if seat is not available', async () => {
+    const requestReservationDto: RequestReservationDto = {
+      seatNumber: 1,
+      date: '2024-01-01',
+    };
+
+    const seat: Seat = {
+      id: 1,
+      userId: 1,
+      seatNumber: 1,
+      isAvailable: false,
+    };
+
+    const getSeatSpy = jest
+      .spyOn(seatReader, 'getSeat')
+      .mockResolvedValue(seat);
+
+    await expect(
+      reservationService.requestReservation({
+        requestReservationDto,
+        userId: 1,
+      }),
+    ).rejects.toThrow(
+      new InternalServerErrorException('Seat is already reserved'),
+    );
+
+    expect(getSeatSpy).toHaveBeenCalledWith({
+      seatNumber: requestReservationDto.seatNumber,
+      dateAvailability: { date: requestReservationDto.date },
+    });
   });
 });
