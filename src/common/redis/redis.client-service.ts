689a4b70abc4732c0650a21b791a029cc87f11df
sunday00 <kind65505256@gmail.com>
2023-12-31 01:36:30 +0900

    :ambulance: redis transaction

--- src/common/redis/redis.client-service.ts
+++ src/common/redis/redis.client-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import Redis from 'ioredis';
+import Redis, { ChainableCommander } from 'ioredis';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 
 @Injectable()
@@ -7,6 +7,8 @@ export class RedisClientService {
   readonly waiting: Redis;
   readonly task: Redis;
   readonly reservation: Redis;
+
+  transaction: { [key: string]: ChainableCommander } | null = {};
 
   constructor(private readonly service: RedisService) {
     this.waiting = this.service.getClient('waiting');
