dbea90a5ff3508bbf47ff43ac06e728cc8da23e3
Andrea Kim <beomyeon.kim@adriel.ai>
2024-01-05 14:44:35 +0900

    chore: add log test

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,23 +4,29 @@ import {
   HealthCheck,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
+import Logger from 'src/common/logger/logger';
 
 @Controller('health')
 export class HealthController {
+  private logger: Logger;
   constructor(
     private health: HealthCheckService,
     private db: TypeOrmHealthIndicator,
-  ) {}
+  ) {
+    this.logger = new Logger('Health');
+  }
 
   @Get()
   @HealthCheck()
   check() {
+    this.logger.info('health checked');
     return this.health.check([]);
   }
 
   @Get('/db')
   @HealthCheck()
   dbcheck() {
+    this.logger.info('db health checked');
     return this.db.pingCheck('database');
   }
 }
